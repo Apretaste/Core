54ba4b1113af49825eaeb8edeb7c31ac820e35e4
Salvi <salvi.pascual@pragres.com>
2015-09-12 19:16:24 -0400

    .gitignore is now working

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ mkdir logs
 mkdir public/ads
 mkdir public/profile
 mkdir public/raffle
+mkdir public/tienda
 chmod 777 temp
 chmod 777 services
 chmod 777 public/ads
 chmod 777 public/profile
 chmod 777 public/raffle
-
+chmod 777 public/tienda
