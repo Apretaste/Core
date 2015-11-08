856e1222d787c93e37aa83af2c9e184a85da09f4
Salvi Pascual <salvi.pascual@pragres.com>
2015-11-08 10:33:47 -0500

    fix buttons in outlook

--- app/plugins/function.button.php
+++ app/plugins/function.button.php
@@ -24,12 +24,10 @@ function smarty_function_button($params, $template)
 	// create and return button
 	return 
 	"<!--[if mso]>
-		<v:roundrect xmlns:v='urn:schemas-microsoft-com:vml' xmlns:w='urn:schemas-microsoft-com:office:word' href='http://litmus.com' style='height:36px;v-text-anchor:middle;width:150px;' arcsize='5%' strokecolor='#EB7035' fillcolor='#EB7035'>
+		<v:roundrect xmlns:v='urn:schemas-microsoft-com:vml' xmlns:w='urn:schemas-microsoft-com:office:word' href='mailto:$validEmailAddress?subject=$href&amp;body=$body' style='height:36px;v-text-anchor:middle;width:150px;' arcsize='5%' strokecolor='#5dbd00' fillcolor='#5EBB47'>
 		<w:anchorlock/>
-		<center style='color:#ffffff;font-family:Helvetica, Arial,sans-serif;font-size:16px;'>I am a button &rarr;</center>
+		<center style='color:#ffffff;font-family:Helvetica, Arial,sans-serif;font-size:16px;'>$caption</center>
 		</v:roundrect>
 	<![endif]-->
 	<a href='mailto:$validEmailAddress?subject=$href&amp;body=$body' style='background-color:#5EBB47;border:1px solid #5dbd00;border-radius:3px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:16px;line-height:44px;text-align:center;text-decoration:none;width:150px;-webkit-text-size-adjust:none;mso-hide:all;'>$caption</a>";
-	
-//	return "<nobr><a href='mailto:$validEmailAddress?subject=$href&amp;body=$body' style='white-space:nowrap;font-size:11pt;font-family:Arial,Helvetica,sans-serif;color:white;text-decoration:none;font-weight:bold;padding:10px;background-color:#5dbd00' target='_blank'>$caption</a></nobr>";
 }
