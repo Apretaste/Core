81c07e131a88594e233f5071a6fc994fb88a5127
salvipascual <salvi.pascual@gmail.com>
2016-01-05 18:56:16 -0500

    convert core into sandbox

--- app/plugins/function.img.php
+++ app/plugins/function.img.php
@@ -10,11 +10,25 @@
 function smarty_function_img($params, $template)
 {
 	// get params
-	$href = basename($params["src"]); 
+	$href = $params["src"];
 	$alt = isset($params["alt"]) ? $params["alt"] : "";
 	$width =  isset($params["width"]) ? "width='{$params["width"]}'" : "";
 	$height =  isset($params["height"]) ? "height='{$params["height"]}'" : "";
 
+	$file = basename($href);
+	$di = \Phalcon\DI\FactoryDefault::getDefault();
+	if($di->get('environment') == "sandbox")
+	{
+		$wwwroot = $di->get('path')['root'];
+		$wwwhttp = $di->get('path')['http'];
+		copy($href, "$wwwroot/public/temp/$file");
+		$destination = "$wwwhttp/temp/$file";
+	}
+	else
+	{
+		$destination = "cid:$file";
+	}
+
 	// create and return image
-	return "<img src='cid:$href' alt='$alt' $width $height />";
+	return "<img src='$destination' alt='$alt' $width $height />";
 }
