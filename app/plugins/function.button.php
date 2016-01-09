8a6a5e2d807aafede53ae7638e0fa5dc268ccaa0
avijaramuk <kumarajiva2015@gmail.com>
2016-01-09 00:14:53 -0500

    improve button smarty plugin

--- app/plugins/function.button.php
+++ app/plugins/function.button.php
@@ -7,37 +7,81 @@
  * @subpackage PluginsFunction
  */
 
-function smarty_function_button($params, $template)
-{
+function smarty_function_button($params, $template) {
 	// get params
 	$href = $params["href"];
 	$caption = $params["caption"];
+	$size = "meddium";
+	if (isset($params["size"]))
+		$size = $params["size"];
 
 	// get the body if exist
-	if(isset($params["body"])) $body = $params["body"];
-	else $body = "Envie+el+correo+tal+y+como+esta,+ya+esta+preparado+para+usted";
+	if (isset($params["body"]))
+		$body = $params["body"];
+	else
+		$body = "Envie+el+correo+tal+y+como+esta,+ya+esta+preparado+para+usted";
 
 	// select the color scheema
 	$color = isset($params["color"]) ? $params["color"] : "green";
-	switch ($color)
-	{
-		case "grey": {$stroke = '#CCCCCC'; $fill = '#E6E6E6'; $text = '#000000'; break;}
-		case "blue": {$stroke = '#2E6DA4'; $fill = '#337AB7'; $text = '#FFFFFF'; break;}
-		case "red": {$stroke = '#D43F3A'; $fill = '#D9534F';  $text = '#FFFFFF'; break;}
-		default: {$stroke = '#5dbd00'; $fill = '#5EBB47'; $text = '#FFFFFF';}
+
+	switch ($color) {
+		case "grey":
+			{
+				$stroke = '#CCCCCC';
+				$fill = '#E6E6E6';
+				$text = '#000000';
+				break;
+			}
+		case "blue":
+			{
+				$stroke = '#2E6DA4';
+				$fill = '#337AB7';
+				$text = '#FFFFFF';
+				break;
+			}
+		case "red":
+			{
+				$stroke = '#D43F3A';
+				$fill = '#D9534F';
+				$text = '#FFFFFF';
+				break;
+			}
+		default:
+			{
+				$stroke = '#5dbd00';
+				$fill = '#5EBB47';
+				$text = '#FFFFFF';
+			}
 	}
 
 	// get a valid apretaste email address
 	$utils = new Utils();
 	$validEmailAddress = $utils->getValidEmailAddress();
 
+	switch ($size) {
+		case "small":
+			$width = 80;
+            $fontsize = 12;
+            $height = 16;
+			break;
+		case "meddium":
+			$width = 150;
+			$height = 36;
+            $fontsize = 16;
+			break;
+		case "large":
+			$width = 220;
+            $fontsize = 24;
+			$height = 48;
+			break;
+	}
+
 	// create and return button
-	return 
-	"<!--[if mso]>
-		<v:roundrect xmlns:v='urn:schemas-microsoft-com:vml' xmlns:w='urn:schemas-microsoft-com:office:word' href='mailto:$validEmailAddress?subject=$href&amp;body=$body' style='height:36px;v-text-anchor:middle;width:150px;' arcsize='5%' strokecolor='$stroke' fillcolor='$fill'>
+	return "<!--[if mso]>
+		<v:roundrect xmlns:v='urn:schemas-microsoft-com:vml' xmlns:w='urn:schemas-microsoft-com:office:word' href='mailto:$validEmailAddress?subject=$href&amp;body=$body' style='height:{$height}px;v-text-anchor:middle;width:{$width}px;' arcsize='5%' strokecolor='$stroke' fillcolor='$fill'>
 		<w:anchorlock/>
-		<center style='color:$text;font-family:Helvetica, Arial,sans-serif;font-size:16px;'>$caption</center>
+		<center style='color:$text;font-family:Helvetica, Arial,sans-serif;font-size:{$fontsize}px;'>$caption</center>
 		</v:roundrect>
 	<![endif]-->
-	<a href='mailto:$validEmailAddress?subject=$href&amp;body=$body' style='background-color:$fill;border:1px solid $stroke;border-radius:3px;color:$text;display:inline-block;font-family:sans-serif;font-size:16px;line-height:44px;text-align:center;text-decoration:none;width:150px;-webkit-text-size-adjust:none;mso-hide:all;'>$caption</a>";
+	<a href='mailto:$validEmailAddress?subject=$href&amp;body=$body' style='background-color:$fill;border:1px solid $stroke;border-radius:3px;color:$text;display:inline-block;font-family:sans-serif;font-size:{$fontsize}px;line-height:44px;text-align:center;text-decoration:none;width:{$width}px;-webkit-text-size-adjust:none;mso-hide:all;'>$caption</a>";
 }
