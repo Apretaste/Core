edc0874217565bd922e699f6ee721a214165e3b4
salvipascual <salvi.pascual@gmail.com>
2016-04-10 20:50:55 -0400

    re-check old emails before inviting or reminding them

--- tasks/RemarketingTask.php
+++ tasks/RemarketingTask.php
@@ -14,7 +14,7 @@ class remarketingTask extends \Phalcon\Cli\Task
 	{
 		// inicialize supporting classes
 		$timeStart  = time();
-		$connetion = new Connection();
+		$connection = new Connection();
 		$email = new Email();
 		$service = new Service();
 		$service->showAds = true;
@@ -31,20 +31,39 @@ class remarketingTask extends \Phalcon\Cli\Task
 
 
 		// people who were invited but never used Apretaste
-		$invitedPeople = $connetion->deepQuery("
+		$invitedPeople = $connection->deepQuery("
 			SELECT invitation_time, email_inviter, email_invited
 			FROM invitations 
 			WHERE used=0 
 			AND DATEDIFF(CURRENT_DATE, invitation_time) > 15 
 			AND email_invited NOT IN (SELECT DISTINCT email from delivery_dropped)
 			AND email_invited NOT IN (SELECT DISTINCT email from remarketing)
-			ORDER BY invitation_time ASC
+			ORDER BY invitation_time DESC
 			LIMIT 200");
 
 		// send the first remarketing
 		$log .= "\nINVITATIONS (".count($invitedPeople).")\n";
 		foreach ($invitedPeople as $person)
 		{
+			// check number of days since the invitation was sent 
+			$datediff = time() - strtotime($person->invitation_time);
+			$daysSinceInvitation = floor($datediff/(60*60*24));
+
+			// validate old invitations to avoid bounces
+			if($daysSinceInvitation > 60)
+			{
+				// re-validate the email
+				$res = $utils->deepValidateEmail($person->email_invited);
+
+				// if response not ok or temporal, unsubscribe and do not email
+				if($res[0] != "ok" && $res[0] != "temporal")
+				{
+					$connection->deepQuery("DELETE FROM invitations WHERE email_invited = '{$person->email_invited}'");
+					$log .= "\t --skiping {$person->email_invited}\n";
+					continue;
+				}
+			}
+
 			// send data to the template
 			$content = array(
 				"date"=>$person->invitation_time,
@@ -63,7 +82,7 @@ class remarketingTask extends \Phalcon\Cli\Task
 			$email->sendEmail($person->email_invited, $subject, $html);
 
 			// insert into remarketing table
-			$connetion->deepQuery("INSERT INTO remarketing(email, type) VALUES ('{$person->email_invited}', 'INVITE')");
+			$connection->deepQuery("INSERT INTO remarketing(email, type) VALUES ('{$person->email_invited}', 'INVITE')");
 
 			// display notifications
 			$log .= "\t{$person->email_invited}\n";
@@ -76,7 +95,7 @@ class remarketingTask extends \Phalcon\Cli\Task
 
 
 		// people missed for the last 30 days and with no remarketing emails unopened
-		$firstReminderPeople = $connetion->deepQuery("
+		$firstReminderPeople = $connection->deepQuery("
 			SELECT email, last_access 
 			FROM person 
 			WHERE active=1 
@@ -89,9 +108,29 @@ class remarketingTask extends \Phalcon\Cli\Task
 		$log .= "\nFIRST REMINDER (".count($firstReminderPeople).")\n";
 		foreach ($firstReminderPeople as $person)
 		{
+			// check number of days since the email was last checked
+			$datediff = time() - strtotime($person->last_access);
+			$daysSinceLastChecked = floor($datediff/(60*60*24));
+
+			// validate old emails to avoid bounces
+			if($daysSinceLastChecked > 60)
+			{
+				// re-validate the email
+				$res = $utils->deepValidateEmail($person->email);
+
+				// if response not ok or temporal, unsubscribe and do not email
+				if($res[0] != "ok" && $res[0] != "temporal")
+				{
+					$utils->unsubscribeFromEmailList($person->email);
+					$connection->deepQuery("UPDATE person SET active=0 WHERE email='{$person->email}'");
+					$log .= "\t --skiping {$person->email}\n";
+					continue;
+				}
+			}
+
 			// get services that changed since last time
 			$sql = "SELECT * FROM service WHERE insertion_date BETWEEN '{$person->last_access}' AND CURRENT_TIMESTAMP AND listed=1";
-			$services = $connetion->deepQuery($sql);
+			$services = $connection->deepQuery($sql);
 
 			// create the variabels to pass to the template
 			$content = array("services"=>$services);
@@ -106,7 +145,7 @@ class remarketingTask extends \Phalcon\Cli\Task
 			$email->sendEmail($person->email, "Se le extranna por Apretaste", $html, $images);
 
 			// move remarketing to the next state and add +1 credits
-			$connetion->deepQuery("
+			$connection->deepQuery("
 				START TRANSACTION;
 				UPDATE person SET credit=credit+1 WHERE email='{$person->email}';
 				INSERT INTO remarketing(email, type) VALUES ('{$person->email}', 'REMINDER1');
@@ -123,7 +162,7 @@ class remarketingTask extends \Phalcon\Cli\Task
 
 
 		// people with REMINDER1 unaswered for the last 30 days, and without REMINDER2 created
-		$secondReminderPeople = $connetion->deepQuery("
+		$secondReminderPeople = $connection->deepQuery("
 			SELECT email
 			FROM remarketing A
 			WHERE type='REMINDER1'
@@ -144,7 +183,7 @@ class remarketingTask extends \Phalcon\Cli\Task
 			$email->sendEmail($person->email, "Hace rato no le veo", $html);
 
 			// move remarketing to the next state and add +1 credits
-			$connetion->deepQuery("
+			$connection->deepQuery("
 				START TRANSACTION;
 				UPDATE person SET credit=credit+1 WHERE email='{$person->email}';
 				INSERT INTO remarketing(email, type) VALUES ('{$person->email}', 'REMINDER2');
@@ -161,7 +200,7 @@ class remarketingTask extends \Phalcon\Cli\Task
 
 
 		// people with REMINDER2 unaswered, sent 30 days ago and not EXCLUDED 
-		$thirdReminderPeople = $connetion->deepQuery("
+		$thirdReminderPeople = $connection->deepQuery("
 			SELECT email
 			FROM remarketing A
 			WHERE type='REMINDER2'
@@ -177,7 +216,7 @@ class remarketingTask extends \Phalcon\Cli\Task
 			$utils->unsubscribeFromEmailList($person->email);
 
 			// move remarketing to the next state and unsubscribe
-			$connetion->deepQuery("
+			$connection->deepQuery("
 				START TRANSACTION;
 				UPDATE person SET active=0 WHERE email='{$person->email}';
 				INSERT INTO remarketing(email, type) VALUES ('{$person->email}', 'EXCLUDED');
@@ -201,6 +240,6 @@ class remarketingTask extends \Phalcon\Cli\Task
 		$logger->close();
 
 		// save the status in the database
-		$connetion->deepQuery("UPDATE task_status SET executed=CURRENT_TIMESTAMP, delay='$timeDiff' WHERE task='remarketing'");
+		$connection->deepQuery("UPDATE task_status SET executed=CURRENT_TIMESTAMP, delay='$timeDiff' WHERE task='remarketing'");
 	}
 }
