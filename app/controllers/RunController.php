288526543e03ff513e3929b7fbcbd6fe5649a663
salvipascual <salvi.pascual@gmail.com>
2016-05-03 16:43:29 -0400

    fix issue with to

--- app/controllers/RunController.php
+++ app/controllers/RunController.php
@@ -135,7 +135,7 @@ class RunController extends Controller
 		// filter email From and To 
 		$pattern = "/(?:[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/";
 		preg_match_all($pattern, $_POST['From'], $emailFrom);
-		preg_match_all($pattern, $_POST['To'], $toFrom);
+		if(isset($_POST['To'])) preg_match_all($pattern, $_POST['To'], $toFrom);
 
 		// get values to the variables
 		$fromEmail = $emailFrom[0][0];
