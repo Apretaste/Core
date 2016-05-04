79d042e036b44247594399dda052dcc957ce7115
salvipascual <salvi.pascual@gmail.com>
2016-05-04 12:24:39 -0400

    add bad queries log

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,13 @@ mkdir public/profile
 mkdir public/raffle
 mkdir public/tienda
 mkdir public/temp
+touch logs/error.log
+touch logs/access.log
+touch logs/badqueries.log
+touch logs/mailgun.log
+touch logs/mandrill.log
+touch logs/remarketing.log
+touch logs/webhook.log
 chmod 777 temp
 chmod 777 temp/templates_c
 chmod 777 temp/cache
@@ -17,3 +24,10 @@ chmod 777 public/profile
 chmod 777 public/raffle
 chmod 777 public/tienda
 chmod 777 public/temp
+chmod 777 logs/error.log
+chmod 777 logs/access.log
+chmod 777 logs/badqueries.log
+chmod 777 logs/mailgun.log
+chmod 777 logs/mandrill.log
+chmod 777 logs/remarketing.log
+chmod 777 logs/webhook.log
