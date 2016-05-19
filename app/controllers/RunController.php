c74ef4013f8b0af9b70021df44465d99ec27ea69
kumahacker <kumahavana@gmail.com>
2016-05-19 14:09:45 -0400

    In-reply-to header

--- app/controllers/RunController.php
+++ app/controllers/RunController.php
@@ -166,8 +166,13 @@ class RunController extends Controller
 		$logger->log("From:$fromEmail, To:$toEmail, Subject:$subject\n".print_r($_POST, true)."\n\n");
 		$logger->close();
 
+		// get message id
+		$messageID = null;
+		if (isset($_POST['Message-Id']))
+			$messageID = $_POST['Message-Id'];
+		
 		// execute the webbook
-		$this->processEmail($fromEmail, $fromName, $toEmail, $subject, $body, $attachments, "mailgun");
+		$this->processEmail($fromEmail, $fromName, $toEmail, $subject, $body, $attachments, "mailgun", $messageID);
 	}
 
 	/**
@@ -181,9 +186,9 @@ class RunController extends Controller
 	 * @param String
 	 * @param Array
 	 * @param Enum mandrill,mailgun
-	 * @param String
+	 * @param String messageID
 	 * */
-	private function processEmail($fromEmail, $fromName, $toEmail, $subject, $body, $attachments, $webhook)
+	private function processEmail($fromEmail, $fromName, $toEmail, $subject, $body, $attachments, $webhook, $messageID)
 	{
 		$connection = new Connection();
 		$utils = new Utils();
@@ -242,7 +247,7 @@ class RunController extends Controller
 		$logger->close();
 
 		// execute the query
-		$this->renderResponse($fromEmail, $subject, $fromName, $body, $attachments, "email");
+		$this->renderResponse($fromEmail, $subject, $fromName, $body, $attachments, "email",$messageID);
 	}
 
 	/**
@@ -255,8 +260,9 @@ class RunController extends Controller
 	 * @param String
 	 * @param Array of Objects {type,content,path}
 	 * @param Enum: html,json,email
+	 * @param string messageID
 	 * */
-	private function renderResponse($email, $subject, $sender="", $body="", $attachments=array(), $format="html")
+	private function renderResponse($email, $subject, $sender="", $body="", $attachments=array(), $format="html", $messageID = null)
 	{
 		// get the time when the service started executing
 		$execStartTime = date("Y-m-d H:i:s");
@@ -491,7 +497,7 @@ class RunController extends Controller
 					$subject = trim(preg_replace('/\'|`/', "", $subject));
 
 					// send the response email
-					$emailSender->sendEmail($emailTo, $subject, $body, $images, $attachments);
+					$emailSender->sendEmail($emailTo, $subject, $body, $images, $attachments, $messageID);
 				}
 			}
 
