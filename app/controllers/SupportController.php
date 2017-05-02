b7c8f663ebfa1803da54f84113b5615c7ac0d442
salvipascual <salvi.pascual@gmail.com>
2017-05-02 19:02:10 -0400

    fix error sending outside ap

--- app/controllers/SupportController.php
+++ app/controllers/SupportController.php
@@ -7,7 +7,7 @@ class SupportController extends Controller
 	// do not let anonymous users pass
 	public function initialize(){
 		$security = new Security();
-//		$security->enforceLogin();
+		$security->enforceLogin();
 	}
 
 	/**
