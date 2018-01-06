4da34aaa38d0e0ad8008ff81826c760a40eaa5db
kumahacker <kumahavana@gmail.com>
2018-01-06 13:26:42 -0500

    update

--- classes/Debug.php
+++ classes/Debug.php
@@ -7,6 +7,12 @@
  */
 class Debug {
 
+	/**
+	 * Readable exception
+	 * 
+	 * @param Exception $e
+	 * @return string
+	 */
 	static function getReadableException(Exception $e)
 	{
 		return "EXCEPTION: {$e->getMessage()} in {$e->getFile()}:{$e->getLine()} \n {$e->getTraceAsString()}";
