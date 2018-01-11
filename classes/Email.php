0ec60025afdb46d1fb694e333c4755f53cd78e80
kumahacker <kumahavana@gmail.com>
2018-01-10 19:52:29 -0500

    fix image extension

--- classes/Email.php
+++ classes/Email.php
@@ -414,6 +414,7 @@ class Email
 				{
 					// thumbnail the image or use thumbnail cache
 					$thumbnail = $utils->getTempDir() . "thumbnails/" . basename($file);
+					$thumbnail = pathinfo($thumbnail, PATHINFO_FILENAME) . ".$format";
 
 					if( ! file_exists($thumbnail)) {
 						$utils->optimizeImage($file, $width, "", $qualityImage, $format, $thumbnail);
