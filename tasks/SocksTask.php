7d3affc2c4750ef09feb4c4c92dab71f2a4c7a40
kumahacker <kumahavana@gmail.com>
2018-01-22 15:37:26 -0500

    keep first socks alive

--- tasks/SocksTask.php
+++ tasks/SocksTask.php
@@ -6,6 +6,39 @@ class SocksTask extends \Phalcon\Cli\Task
 
 	public function mainAction()
 	{
+		$di = \Phalcon\DI\FactoryDefault::getDefault();
+		$www_root = $di->get('path')['root'];
+		$configFile = "$www_root/configs/socks.json";
+
+		$newproxies = [];
+
+		if (file_exists($configFile)) {
+
+			echo "Verifying current config....\n";
+
+			$proxies = file_get_contents("$www_root/configs/socks.json");
+			$proxies = json_decode($proxies);
+
+			foreach ($proxies as $proxy) {
+				if (is_object($proxy)) $proxy = get_object_vars($proxy);
+
+				echo "Verifying current config host: {$proxy['host']}:{$proxy['port']} ...";
+
+				$kk = new Krawler("http://example.com");
+				$result = $kk->getRemoteContent("http://example.com", $info, [
+					"host" => "{$proxy['host']}:{$proxy['port']}",
+					"type" => CURLPROXY_SOCKS5
+				]);
+
+				if ($result !== false)
+					$newproxies[] = $proxy;
+			}
+		}
+
+		SocksTask::$hosts[] = $newproxies;
+
+		echo "Searching for new proxies....\n";
+
 		$k = new Krawler("https://www.socks-proxy.net/");
 		$k->filter("#proxylisttable")->filter("tr")->each(function ($item, $i) {
 			$html = $item->html();
@@ -34,10 +67,6 @@ class SocksTask extends \Phalcon\Cli\Task
 			}
 		});
 
-
-		// get a path to the root folder
-		$di = \Phalcon\DI\FactoryDefault::getDefault();
-		$wwwroot = $di->get('path')['root'];
-		file_put_contents("$wwwroot/configs/socks.json", json_encode(SocksTask::$hosts, JSON_PRETTY_PRINT));
+		file_put_contents($configFile, json_encode(SocksTask::$hosts, JSON_PRETTY_PRINT));
 	}
 }
