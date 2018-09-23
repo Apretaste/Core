cd2de97450f5f3ae66c38208ea7fbefbe2dc8e19
Salvi Pascual <salvi.pascual@gmail.com>
2018-09-23 00:11:04 -0400

    fix little issues

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -10,47 +10,26 @@ class IndexController extends Controller
 		$lang = $this->dispatcher->getParam("lang");
 		if(empty($lang)) $lang = "es";
 
-		// get path to the cache file
-		$utils = new Utils();
-		$cacheFile = $utils->getTempDir().date("Ym").".cache";
+		// get visitors
+		$currentMonth = date("Y-m");
+		$visits = Connection::query("
+			SELECT value, dated
+			FROM summary 
+			WHERE label = 'monthly_gross_traffic'
+			AND dated < '$currentMonth'
+			ORDER BY dated DESC
+			LIMIT 5");
 
-		// load from cache if exist
-		if(file_exists($cacheFile))
-		{
-			$variables = explode("|", file_get_contents($cacheFile));
-			$visitors = unserialize($variables[0]);
-			$visitorsPerMonth = $variables[1];
-		}
-		// else get from the database
-		else
-		{
-			// get visitors
-			$connection = new Connection();
-			$visits = $connection->query("
-				SELECT
-				count(id) as received,
-				DATE_FORMAT(request_date,'%Y-%m') as inserted
-				FROM delivery
-				GROUP BY DATE_FORMAT(request_date,'%Y-%m')
-				HAVING inserted <> DATE_FORMAT(curdate(), '%Y-%m')
-				ORDER BY inserted DESC
-				LIMIT 5");
-
-			// format data for the chart
-			$visitors = [];
-			$visitorsPerMonth = 0;
-			foreach($visits as $visit) {
-				if($visit->received > $visitorsPerMonth) $visitorsPerMonth = $visit->received;
-				$visitors[] = ["date"=>date("M Y", strtotime($visit->inserted)), "emails"=>$visit->received];
-			}
-			$visitors = array_reverse($visitors);
-
-			// create cache file
-			file_put_contents($cacheFile, serialize($visitors)."|$visitorsPerMonth");
+		// format data for the chart
+		$visitors = [];
+		$visitorsPerMonth = 0;
+		foreach($visits as $visit) {
+			if($visitorsPerMonth < $visit->value) $visitorsPerMonth = $visit->value;
+			$visitors[] = ["date"=>$visit->dated, "emails"=>$visit->value];
 		}
 
 		// send data to the view
-		$this->view->visitors = $visitors;
+		$this->view->visitors = array_reverse($visitors);
 		$this->view->visitorsPerMonth = $visitorsPerMonth;
 		$this->view->wwwroot = $this->di->get('path')['root'];
 		$this->view->pick("index/$lang");
