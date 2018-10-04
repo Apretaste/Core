e0a174fa080c4c6ba40e3179da025aec4ef246b4
Ricardo Jimenez <ricardo@apretaste.com>
2018-10-03 21:35:04 -0400

    better

--- app/controllers/RunController.php
+++ app/controllers/RunController.php
@@ -657,8 +657,7 @@ class RunController extends Controller
 	{
 		ignore_user_abort(true);
 		// get data from our webhook
-		$this->callAPWebhook();
-		$this->webhookProcessRequest();
+		if($this->callAPWebhook()) $this->webhookProcessRequest();
 	}
 
 	/**
@@ -673,7 +672,7 @@ class RunController extends Controller
 		$utils = new Utils();
 		$temp = $utils->getTempDir();
 
-		if(!(isset($message['header']['from']) && isset($message['header']['message_id']))) exit;
+		if(!(isset($message['header']['from']) && isset($message['header']['message_id']))) return false;
 
 		// parse the file
 		$messageId = str_replace(array("<",">","'"), "", $message['header']['message_id']);
@@ -711,5 +710,6 @@ class RunController extends Controller
 		$this->toEmail = $toEmail;
 		$this->subject = $subject;
 		$this->body = str_replace("'", "", $body);
+		return true;
 	}
 }
