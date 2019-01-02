3169080a7b5b23789a58858c92900a0e63b4f7f9
kumahacker <kumahavana@gmail.com>
2019-01-01 23:07:29 -0500

    better audience apretin

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -472,10 +472,8 @@ class ApiController extends Controller
           $r2 = Connection::query("SELECT count(*) as total FROM person where active = 1;");
           $r3 = Connection::query("SELECT count(*) as total FROM person;");
 
-          $sendMessage($chat_id, "En esta semana Apretaste ha tenido un total de <strong>{$r[0]->total} accesos</strong> ".
-                                         "tanto por web como por email, dados por <strong>{$r1[0]->total} usuarios</strong>. ".
-                                         "Actualmente hay <strong>{$r2[0]->total} usuarios activos</strong> de un total <strong>{$r3[0]->total}</strong> que ".
-                                         "alguna vez han accedido.", $token);
+          $sendMessage($chat_id, "En esta semana han accedido <strong>{$r1[0]->total} usuarios</strong> a Apretaste. ".
+                                         "Actualmente hay <strong>{$r2[0]->total} usuarios activos</strong>.", $token);
           return;
         }
 /*
